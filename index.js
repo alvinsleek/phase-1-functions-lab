6e5529fc4454d8b01a3b99b3a715afdc7c945529
Alvin Mithamo <117805605+alvinsleek@users.noreply.github.com>
2022-12-09 13:35:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-const headquaters = 42;
-
-function distanceFromHqInBlocks(passlocation) {
-    if (passlocation > 42 )}
-        return (passengerlocation - headquaters);
-function distanceFromHqInBlocks(passLocation) {
-    if (passengerLocation > 42 ){
-        return (passLocation - headquaters);
-    }
-
-    else{
-        return(headquaters - passlocation);
-        
-    }
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(pickupLocation - 42);
 }
 
-function distanceFromHqInFeet(passlocation){
-    const distanceTravelledInFeet = distanceFromHqInBlocks(passlocation);
-function distanceFromHqInFeet(passLocation){
-    const distanceTravelledInFeet = distanceFromHqInBlocks(passLocation);
-    return distanceTravelledInFeet * 264;
+function distanceFromHqInFeet(pickupLocation) {
+  var blocks = distanceFromHqInBlocks(pickupLocation);
+  return blocks * 264;
+
+function distanceTravelledInFeet(start, destination) {
+  return (destination - start) * 264;
 }
 
-function distanceTravelledInFeet(distanceTravelledInFeet, distanceFromHqInBlocks
-    return distance * 264;
+function calculatesFarePrice(start, destination) {
+  var distance = distanceTravelledInFeet(start, destination);
+  if (distance > 2500) {
+    return "cannot travel that far";
+  } 
+  else if (distance > 2000) {
+    return "$15.00";
+  } 
+  else if (distance > 400) {
+    return "$" + (distance * 0.02).toFixed(2);
+  } 
+  else {
+    return "$0.00";
+  }
 }
-
-function calculatesFarePrice(start,end){
-    const distance = distanceTravelledInFeet(start, end);}
-function calculatesFarePrice(start,destination){
-    const distance = distanceTravelledInFeet(start, destination);}
-    let fare;
-    if (distance <= 400) {
-        return fare = 0;
-    }
